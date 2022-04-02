b2496470cc447bf63c500eeca83b5b8639f793a7
Guy <chamberlainwebberj@gmail.com>
2022-04-02 18:29:04 +0100

    Created WorldGrid class

--- Ludum Dare 50/Assets/Scripts/WorldGrid.cs
+++ Ludum Dare 50/Assets/Scripts/WorldGrid.cs
@@ -1,18 +1,42 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WorldGrid : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static WorldGrid _instance;
+    
+    private const int GRID_SIZE = 10;
 
-    // Update is called once per frame
-    void Update()
+    private List<WorldTile> worldTiles;
+
+    #region Singleton
+    private void Awake()
     {
+        if (_instance == null)
+        {
+            _instance = this;
+        } else if (_instance != this)
+        {
+            Debug.Log("Instance already exists, destroying object!");
+            Destroy(this);
+        }
+    }
+    #endregion
+   
+    private void Start()
+    {
+        // Create the world tiles for the game.
+        int currentRow = 0;
         
+        for (int i = 1; i <= GRID_SIZE * GRID_SIZE; i++)
+        {
+            if (i % GRID_SIZE == 0)
+            {
+                currentRow++;
+            }
+
+            WorldTile newWorldTile = new WorldTile(i, currentRow);
+            worldTiles.Add(newWorldTile);
+        }
     }
 }
