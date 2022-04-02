bbc848d0b28e9143518159bcf6b43a5285daecba
Guy <chamberlainwebberj@gmail.com>
2022-04-02 19:03:56 +0100

    Added MonoBehaviour to the world tile

--- Ludum Dare 50/Assets/Scripts/WorldTile.cs
+++ Ludum Dare 50/Assets/Scripts/WorldTile.cs
@@ -18,7 +18,7 @@ public enum TileResourceType
     FOOD
 }
 
-public class WorldTile
+public class WorldTile : MonoBehaviour
 {
     // The X and Y dimensions for a tile.
     private const int TILE_SIZE = 100;
