f4e0d791a1609c3858d5b9ab9f75709ecb0fb012
Guy <chamberlainwebberj@gmail.com>
2022-04-02 19:41:49 +0100

    Implemented the build world method, so tiles are added to the scene

--- Ludum Dare 50/Assets/Scripts/WorldGrid.cs
+++ Ludum Dare 50/Assets/Scripts/WorldGrid.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 public class WorldGrid : MonoBehaviour
 {
     public static WorldGrid _instance;
-    
-    private const int GRID_SIZE = 10;
 
-    private List<WorldTile> worldTiles;
+    private Vector2 _startingCoords;
+    
+    [SerializeField] private int _gridSize;
+    [SerializeField] private GameObject _worldTilePrefab;
+
+    private List<Vector2> _tileCoords;
 
     #region Singleton
     private void Awake()
@@ -26,17 +29,34 @@ public class WorldGrid : MonoBehaviour
     private void Start()
     {
         // Create the world tiles for the game.
+        _startingCoords = Vector2.zero;
+        _tileCoords = new List<Vector2>();
+        
         int currentRow = 0;
         
-        for (int i = 1; i <= GRID_SIZE * GRID_SIZE; i++)
+        for (int i = 0; i < _gridSize * _gridSize; i++)
         {
-            if (i % GRID_SIZE == 0)
+            if (i % _gridSize == 0)
             {
                 currentRow++;
             }
 
-            WorldTile newWorldTile = new WorldTile(i, currentRow);
-            worldTiles.Add(newWorldTile);
+            _tileCoords.Add(new Vector2(i % _gridSize, currentRow));
+        }
+
+        BuildWorld();
+    }
+
+    private void BuildWorld()
+    {
+        Debug.Log("Building the world");
+        
+        foreach (Vector2 coords in _tileCoords)
+        {
+            Vector3 convertedCoords = new Vector3(coords.x, coords.y, 0f);
+            GameObject newTile = Instantiate(_worldTilePrefab, convertedCoords, transform.rotation);
+            
+            // Set random tile attributes here.
         }
     }
 }
