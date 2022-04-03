114f754b98373cf801318c121b0a5f88231a7756
Guy <chamberlainwebberj@gmail.com>
2022-04-03 16:14:15 +0100

    Polished the camera movement slightly

--- Ludum Dare 50/Assets/Scripts/WorldGridMovement.cs
+++ Ludum Dare 50/Assets/Scripts/WorldGridMovement.cs
@@ -11,6 +11,8 @@ public class WorldGridMovement : MonoBehaviour
     private void Start()
     {
         _mainCamera = Camera.main;
+
+        _dragOrigin = Vector3.zero;
     }
 
     private void Update()
@@ -18,15 +20,16 @@ public class WorldGridMovement : MonoBehaviour
         if (Input.GetMouseButtonDown(2))
         {
             _dragOrigin = Input.mousePosition;
-            return;
         }
 
         if (Input.GetMouseButton(2))
         {
-            Vector3 pos = _mainCamera.ScreenToViewportPoint(Input.mousePosition - _dragOrigin);
+            Vector3 pos = _mainCamera.ScreenToViewportPoint(_dragOrigin - Input.mousePosition);
             Vector3 move = new Vector3(pos.x * _mouseDragSpeed, pos.y * _mouseDragSpeed, 0);
+
+            Vector3 deltaPos = transform.position - move;
             
-            transform.Translate(move, Space.World);
+            transform.position = deltaPos;
         }
     }
 }
