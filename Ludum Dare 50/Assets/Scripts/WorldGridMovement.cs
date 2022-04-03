49a106abb5742b9915a33d31973d94dd2a72499c
Guy <chamberlainwebberj@gmail.com>
2022-04-03 16:44:25 +0100

    Added mouse zoom movement

--- Ludum Dare 50/Assets/Scripts/WorldGridMovement.cs
+++ Ludum Dare 50/Assets/Scripts/WorldGridMovement.cs
@@ -6,8 +6,14 @@ public class WorldGridMovement : MonoBehaviour
 
     private Vector3 _dragOrigin;
     private Vector3 _lastDragOrigin;
-    
+
+    [Header("Mouse drag movement")]
     [SerializeField] private float _mouseDragSpeed;
+    
+    [Header("Mouse zoom movement")]
+    [SerializeField] private float _minCameraZoom = 5f;
+    [SerializeField] private float _maxCameraZoom = 0.5f;
+    [SerializeField] private float _cameraZoomStep = 0.5f;
     
     private void Start()
     {
@@ -18,6 +24,8 @@ public class WorldGridMovement : MonoBehaviour
 
     private void Update()
     {
+        // Camera dragging movement
+        
         if (Input.GetMouseButtonDown(2))
         {
             _lastDragOrigin = Input.mousePosition;
@@ -32,6 +40,24 @@ public class WorldGridMovement : MonoBehaviour
             transform.position += (deltaPos * _mouseDragSpeed * Time.deltaTime);
 
             _lastDragOrigin = _dragOrigin;
+        }
+
+        // Camera zooming
+        
+        if (Input.mouseScrollDelta.y < 0)
+        {
+            _mainCamera.orthographicSize += _cameraZoomStep;
+        } else if (Input.mouseScrollDelta.y > 0)
+        {
+            _mainCamera.orthographicSize -= _cameraZoomStep;
+        }
+
+        if (_mainCamera.orthographicSize > _minCameraZoom)
+        {
+            _mainCamera.orthographicSize = _minCameraZoom;
+        } else if (_mainCamera.orthographicSize < _maxCameraZoom)
+        {
+            _mainCamera.orthographicSize = _maxCameraZoom;
         }
     }
 }
