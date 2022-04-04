a9f455da78ad2fdd7092ae620cfa13f17358dd5c
Guy <chamberlainwebberj@gmail.com>
2022-04-04 19:53:37 +0100

    Added constant income values for each tile area

--- Ludum Dare 50/Assets/Scripts/Constants.cs
+++ Ludum Dare 50/Assets/Scripts/Constants.cs
@@ -1,7 +1,12 @@
-﻿namespace DefaultNamespace
+﻿using UnityEngine;
+
+public class Constants : MonoBehaviour
 {
-    public class Constants
-    {
-        
-    }
+    public const int FARMLAND_TURN_INCOME = 5;
+    public const int FOREST_TURN_INCOME = 2;
+    public const int MOUNTAIN_TURN_INCOME = 3;
+    public const int FORTRESS_TURN_INCOME = 8;
+    public const int CITY_TURN_INCOME = 10;
+    public const int MINE_TURN_INCOME = 7;
+    public const int PLAINS_TURN_INCOME = 1;
 }
