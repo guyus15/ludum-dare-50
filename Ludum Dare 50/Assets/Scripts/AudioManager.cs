b0a42dc3b828f5051a86abf9c3274a3c75aa2942
Guy <chamberlainwebberj@gmail.com>
2022-04-05 01:41:30 +0100

    Added sound and audio system

---
+++ Ludum Dare 50/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    // Sound effects
+    
+    [SerializeField] private AudioSource _buttonEffect;
+    [SerializeField] private AudioSource _swordSheathEffect;
+    [SerializeField] private AudioSource _swordFightSoundEffect;
+    [SerializeField] private AudioSource _battleSoundEffect;
+
+    private void Start()
+    {
+        
+    }
+}
